e951b69e9b72831c1f9d28415dc64e386258244b
Excel619 <excel8392@gmail.com>
2023-06-07 15:23:56 -0600

    Cancelled chat message if message is already cancelled

--- src/main/java/com/runicrealms/listener/PlayerMessageListener.java
+++ src/main/java/com/runicrealms/listener/PlayerMessageListener.java
@@ -9,6 +9,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
@@ -23,8 +24,9 @@ import java.util.List;
  */
 public class PlayerMessageListener implements Listener {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGH)
     public void onPlayerChat(AsyncPlayerChatEvent event) {
+        if (event.isCancelled()) return;
         event.setCancelled(true);
 
         ChatChannel channel = RunicChat.getRunicChatAPI().getPlayerChatChannel(event.getPlayer());
